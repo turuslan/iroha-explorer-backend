527057afdbce06c4656a22e94f9b4826ac36b806
turuslan <turuslan.devbox@gmail.com>
2019-08-22 12:13:35 +0300

    refactor table creation

--- files/postgres.sql
+++ files/postgres.sql
@@ -5,48 +5,38 @@ BEGIN;
 
 CREATE TABLE block (
   protobuf BYTEA NOT NULL,
-  height BIGINT NOT NULL,
+  height BIGINT NOT NULL UNIQUE,
   created_time TIMESTAMP WITH TIME ZONE NOT NULL,
   transaction_count INT NOT NULL
 );
-
-CREATE UNIQUE INDEX ON block(height);
 
 
 
 CREATE TABLE transaction (
   protobuf BYTEA NOT NULL,
-  index BIGINT NOT NULL,
-  hash CHAR(64) NOT NULL,
+  index BIGINT NOT NULL UNIQUE,
+  hash CHAR(64) NOT NULL UNIQUE,
   creator_domain VARCHAR(255) NOT NULL,
   block_height BIGINT NOT NULL,
   time TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
-CREATE UNIQUE INDEX ON transaction(index);
-
 
 
 CREATE TABLE account (
-  index INT NOT NULL,
-  id VARCHAR(288) NOT NULL,
+  index INT NOT NULL UNIQUE,
+  id VARCHAR(288) NOT NULL UNIQUE,
   quorum INT NOT NULL
 );
-
-CREATE UNIQUE INDEX ON account(id);
-CREATE UNIQUE INDEX ON account(index);
 
 
 
 CREATE TABLE peer (
-  index INT NOT NULL,
-  address VARCHAR(261) NOT NULL,
-  public_key VARCHAR NOT NULL
+  index INT NOT NULL UNIQUE,
+  address VARCHAR(261) NOT NULL UNIQUE,
+  public_key VARCHAR NOT NULL UNIQUE
 );
 
-CREATE UNIQUE INDEX ON peer(index);
-CREATE UNIQUE INDEX ON peer(address);
-CREATE UNIQUE INDEX ON peer(public_key);
 
 
 
