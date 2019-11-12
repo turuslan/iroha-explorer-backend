b3d34e05e426c5cadfd5bdfbe88473dc767a998b
turuslan <turuslan.devbox@gmail.com>
2019-11-12 10:38:58 +0300

    rename frontend dir

--- src/files.ts
+++ src/files.ts
@@ -11,4 +11,4 @@ export const postgresSql = read('files/postgres.sql');
 
 export const docPath = resolve(root, 'doc');
 
-export const frontendPath = resolve(root, 'build');
+export const frontendPath = resolve(root, 'frontend');
