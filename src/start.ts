a61d8cdd8f616f774d461b172764dc527204082f
turuslan <turuslan.devbox@gmail.com>
2019-11-15 11:51:11 +0300

    start check db connect

--- src/start.ts
+++ src/start.ts
@@ -1,11 +1,14 @@
 import './logger';
 
 import config from './config';
+import * as db from './db';
 import * as init from './init';
 import * as server from './server';
 import * as sync from './sync';
 
 async function main() {
+  await db.connect();
+
   await init.main();
 
   // tslint:disable-next-line:no-floating-promises
